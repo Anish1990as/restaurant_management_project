c77445f826b332ef4a322c4fcd3bebf0714dc4e4
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-10 06:59:47 +0530

    26538

--- home/views.py
+++ home/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.conf import settings
+from .models import Restaurant
 
 def home(request):
-    restaurent_name = settings.RESTAURENT_NAME 
+
+    if Restaurant.objects.exists():
+     restaurent_name = Restaurant.objects.first().name 
+    else:
+        restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'Restaurant Name')
+
     return render(request, 'home.html',{'restaurent_name': restaurent_name})
