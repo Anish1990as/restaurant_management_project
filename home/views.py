10d32ed273a9b400c856d6bc1b047b4d8cde1349
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-10 06:49:56 +0530

    26538

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.conf import settings
+
+def home(request):
+    restaurent_name = settings.RESTAURENT_NAME 
+    return render(request, 'home.html',{'restaurent_name': restaurent_name})
