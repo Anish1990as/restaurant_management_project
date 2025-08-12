8cea4694095fbef47fcd443f2a389a511f2e6ac7
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-12 12:59:00 +0530

    28667

--- home/models.py
+++ home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
