096fa68787c02456d74df64d14740afe0f22aef7
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-12 09:43:47 +0530

    28667

--- home/views.py
+++ home/views.py
@@ -3,19 +3,13 @@ from django.conf import settings
 from .models import Restaurant
 
 def home(request):
-    """Homepage View"""
-    # Restaurant ka naam get karo
-    if Restaurant.objects.exists():
-        restaurant_name = Restaurant.objects.first().name
-    else:
-        restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'Default Restaurant')
-
-    phone_number = getattr(settings, 'RESTAURANT_PHONE', 'Not Available')
-
-    return render(request, 'home/home.html', {
+    
+    restaurant_name = getattr(settings, 'RESTAURANT_NAME', 'My Restaurant')
+    phone_number = getattr(settings, 'RESTAURANT_PHONE', '+91 0000000000')
+    return render(request, 'home.html', {
         'restaurant_name': restaurant_name,
         'phone_number': phone_number
-    })
+    
 
 def about(request):
      
