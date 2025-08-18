6ce4c8ba674cf0c29b6c8d7d10da280986106431
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-18 12:14:30 +0530

    34526

--- home/models.py
+++ home/models.py
@@ -6,3 +6,14 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+from django.db import models
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.email})"
