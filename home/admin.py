ee94bc9c85580807c0cd1e5f97ab0732d5151b75
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-19 12:05:59 +0530

    34524

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Restaurant, MenuItem, Feedback
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone')
+    search_fields = ('name', 'phone')
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name', 'description')
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    search_fields = ('name', 'email', 'message')
+
+ 
