107e8cf64868b227805ed1179136729f1c52b733
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-19 12:09:26 +0530

    34524

--- home/models.py
+++ home/models.py
@@ -7,13 +7,21 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.name
 
-from django.db import models
 
 class Feedback(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(null=True, blank=True)  # allow null
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} ({self.email})"
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
