75f46ebb8e0774ad2e028e1aad7823a8b072aa42
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-22 16:00:17 +0530

    37599

--- restaurant_management/urls.py
+++ restaurant_management/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('api/accounts/',include('account.urls')),
     path('api/products/',include('products.urls')),
     path('api/orders/',include('orders.urls')),
+    
 ]
 
 # custom error handlers
