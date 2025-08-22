e3ca3203fae2709267d24e87cba505d211977fcb
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-22 15:26:48 +0530

    37599

--- home/urls.py
+++ home/urls.py
@@ -1,18 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import *
-from . import views
-
+from . import views   
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", admin.site.urls),    
     path('', views.home, name='home'),
-     
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('reservations/', views.reservations, name='reservations'),
-    path('feedback/', views.feedback, name='feedback'),
-    path('feedback/thanks/', views.feedback_thanks, name='feedback_thanks'),
+    path("feedback/", views.feedback, name="feedback"),
+    path("feedback_thanks/", views.feedback_thanks, name="feedback_thanks"),
     path('search/', views.search_menu, name='search_menu'),
-    path('menu/', views.menu_list, name='menu'),
+    path("menu/", views.menu_list, name="menu_list"),
+     
 ]
+ 
