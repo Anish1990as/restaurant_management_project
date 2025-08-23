cf0aedbc4a2eb31e40631b788aeb0682fdd384e6
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-23 14:18:17 +0530

    37596

--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+ from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
-# Create your views here.
+@login_required
+def profile_view(request):
+    profile, created = UserProfile.objects.get_or_create(user=request.user)
+
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect("profile")  # same page reload
+    else:
+        form = UserProfileForm(instance=profile)
+
+    return render(request, "account/profile.html", {"form": form})
+
+@login_required
+def profile_view(request):
+    return render(request, 'accounts/profile.html')
