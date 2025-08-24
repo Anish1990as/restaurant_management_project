a9715470397c58ee6bbe2c336137f5ee9331292b
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-24 10:49:01 +0530

    37597

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Restaurant, MenuItem, Feedback
+from .models import Restaurant, MenuItem, Feedback, Order    
+
 
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
@@ -9,13 +10,20 @@ class RestaurantAdmin(admin.ModelAdmin):
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
-    search_fields = ('name', 'description')
-
+    list_display = ("id", "name", "price")  
+    search_fields = ("name",)  
+    list_filter = ("price",)
+    
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     search_fields = ('name', 'email', 'message')
 
+ 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer_name", "menu_item", "quantity", "created_at")
+    search_fields = ("customer_name",)
+    list_filter = ("created_at",)
  
