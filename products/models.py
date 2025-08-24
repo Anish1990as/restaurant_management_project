912ecc18b2b641306e872c8a559b7a345b4a4d9a
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-24 21:33:54 +0530

    37600

--- products/models.py
+++ products/models.py
@@ -9,7 +9,7 @@ class Item(models.Model):
     def __str__(self):
         return str(self.item_name) 
 
-class Menu(models.Model):
+class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
