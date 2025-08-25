dde089ca7fc64ca68059497b02c5f0224fd85a5c
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-25 11:46:35 +0530

    38647

--- home/models.py
+++ home/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+ from django.db import models
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=200)
@@ -24,6 +24,8 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
 
 class Order(models.Model):   
     customer_name = models.CharField(max_length=100)
@@ -33,3 +35,22 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order #{self.id} - {self.customer_name} ({self.menu_item.name} x{self.quantity})"
+
+ 
+class Order(models.Model):
+    menu_item = models.ForeignKey(
+        'MenuItem',
+        on_delete=models.CASCADE,
+        related_name="home_orders"    
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+ 
+ 
+
+class RestaurantInfo(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
