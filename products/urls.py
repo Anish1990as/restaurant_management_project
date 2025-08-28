627d58d9f08984e64d92f5dceea36bab03eb2678
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-28 22:13:23 +0530

    39387

--- products/urls.py
+++ products/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list-create'),
     path("menu/", views.menu_list, name="menu"), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
