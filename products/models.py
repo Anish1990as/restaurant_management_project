b9d1462e3240dae958b4faaa0e13b495d81bc4bb
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-28 22:09:14 +0530

    39387

--- products/models.py
+++ products/models.py
@@ -12,7 +12,8 @@ class Item(models.Model):
 class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menu_items")
     image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
 
     def __str__(self):
