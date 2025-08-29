49afddcca7b10d24c6467ed6df9e0a1e36a58b6e
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-29 23:38:50 +0530

    39388

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list-create'),
     path("menu/", views.menu_list, name="menu"), 
+    path('menu/', menu_view, name='menu'),
 ]
 
 if settings.DEBUG:
