4f654878d823726fac374ead18227a6fdeaf5167
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-29 17:25:54 +0530

    39392

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Restaurant, MenuItem, Feedback, Order    
-
+from .models import ContactSubmission
  
  
 @admin.register(Restaurant)
@@ -21,3 +21,7 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     search_fields = ('name', 'email', 'message')
 
+@admin.register(ContactSubmission)
+class ContactSubmissionAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "created_at")
+    search_fields = ("name", "email")
