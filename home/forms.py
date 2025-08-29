4c5db58b8eb841ba3cbe36cc2d549a6b3294a9ed
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-08-29 16:41:16 +0530

    39389

--- home/forms.py
+++ home/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Feedback
+from .models import ContactSubmission
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['name', 'email', 'message']
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = ["name", "email"]
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "Your name", "class": "form-control"}),
+            "email": forms.EmailInput(attrs={"placeholder": "you@example.com", "class": "form-control"}),
+        }
