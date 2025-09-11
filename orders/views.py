b0cb1b769b21f72b95d107e428e034401bad42a4
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-11 10:21:04 +0530

    43329

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+
+def order_confirmation(request):
+    # Generate a sample order number (you can replace this with actual order id from DB)
+    order_number = random.randint(10000, 99999)
+    context = {
+        'order_number': order_number
+    }
+    return render(request, 'orders/order_confirmation.html', context)
