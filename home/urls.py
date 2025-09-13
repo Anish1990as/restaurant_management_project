bbac5572668cef2106636245d3080f917300a401
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-13 18:10:12 +0530

    44313

--- home/urls.py
+++ home/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('place-order/', views.place_order, name='place_order'),
+    
 ]
  
