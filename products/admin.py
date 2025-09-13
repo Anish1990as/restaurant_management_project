c933a7a61a7b3d4b1c2746e89ec90897c1a8195a
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-13 18:09:06 +0530

    44313

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import *
- 
+from .models import TodaysSpecial
+
+
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
  
@@ -9,3 +11,9 @@ admin.site.register(Item,ItemAdmin,Menu)
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'restaurant', 'price', 'image')
+
+
+@admin.register(TodaysSpecial)
+class TodaysSpecialAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    search_fields = ('name',)
