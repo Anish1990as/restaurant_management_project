8f9a2eb7d907ab796cc830f4317e5740f147696c
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-13 18:08:39 +0530

    44313

--- products/models.py
+++ products/models.py
@@ -13,9 +13,17 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="menu_items")
-    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
- 
+
+
+class TodaysSpecial(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateField(auto_now_add=True)  # Automatically store the date
+
+    def __str__(self):
+        return self.name
