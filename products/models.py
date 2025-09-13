3b225e36441607b5c5d64bb2694af838b210eaef
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-13 18:15:07 +0530

    44317

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+ 
+ 
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -23,7 +23,14 @@ class TodaysSpecial(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    created_at = models.DateField(auto_now_add=True)  # Automatically store the date
-
+    image = models.ImageField(upload_to="specials/", blank=True, null=True)
+    created_at = models.DateField(auto_now_add=True)  
     def __str__(self):
         return self.name
+
+class HomepageBanner(models.Model):
+    title = models.CharField(max_length=200, blank=True, null=True)
+    background_image = models.ImageField(upload_to="banners/")
+
+    def __str__(self):
+        return self.title if self.title else "Homepage Banner"
