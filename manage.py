96f1b6ce073a3def539160bdb6e91adf0b61bc81
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-18 00:04:19 +0530

    44316

--- manage.py
+++ manage.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-
 def main():
-    """Run administrative tasks."""
+    """Run administrative tasks (same as manage.py)."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Make sure it's installed and "
-            "available on your PYTHONPATH environment variable. Did you "
-            "forget to activate a virtual environment?"
+            "available on your PYTHONPATH environment variable. "
+            "Did you forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
