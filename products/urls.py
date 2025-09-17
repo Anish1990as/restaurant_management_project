0b893b40c80b1ff5b78cbd66eab8b231b261b167
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-17 23:37:26 +0530

    44314

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
 from .views import *
+from . import views
 
 urlpatterns = [
     path("", include("home.urls")),
     path('items/', ItemView.as_view(), name='item-list-create'),
     path('menu/', views.menu_list, name='menu'), 
     path('menu/', views.menu_view, name='menu'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('cart/add/<int:item_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
 
 if settings.DEBUG:
