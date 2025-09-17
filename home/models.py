2e521dfbf777a40fc0f3043ae6eff90f307271be
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-17 23:53:36 +0530

    44316

--- home/models.py
+++ home/models.py
@@ -86,3 +86,11 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    feedback_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback from {self.name}"
