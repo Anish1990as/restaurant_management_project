82477b8f2f43afd4072a298409b4c7fddf8406f0
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-20 00:13:37 +0530

    48124

--- orders/models.py
+++ orders/models.py
@@ -40,3 +40,10 @@ class CartItem(models.Model):
 
     def __str__(self):
         return f"{self.product.name} ({self.quantity})" 
+
+
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
