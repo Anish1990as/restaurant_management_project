d072350cd5f62f157af2986881667070819e46f9
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-20 00:13:50 +0530

    48124

---
+++ orders/__init__.py
@@ -0,0 +1,8 @@
+# orders/__init__.py
+
+PENDING = "Pending"
+PROCESSING = "Processing"
+COMPLETED = "Completed"
+CANCELLED = "Cancelled"
+
+DEFAULT_STATUSES = [PENDING, PROCESSING, COMPLETED, CANCELLED]
