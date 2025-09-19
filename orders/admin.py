190ff10d404228c9a4cdaa0044d9f0b8c5d6a43a
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-20 00:01:07 +0530

    48125

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Coupon
+from .utils import generate_coupon_code
 
-# Register your models here.
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_percent', 'valid_from', 'valid_to', 'active')
+    search_fields = ('code',)
+    list_filter = ('active', 'valid_from', 'valid_to')
+
+    # Auto-generate code if not set
+    def save_model(self, request, obj, form, change):
+        if not obj.code:
+            obj.code = generate_coupon_code()
+        super().save_model(request, obj, form, change)
