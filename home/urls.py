c988e86dd56e2ed06afd11614a1efa57f0850873
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-19 23:50:35 +0530

    48126

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views   
 from .views import contact_view
 from django.contrib.auth import views as auth_views
+from .views import MenuCategoryListView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),    
@@ -22,6 +24,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('place-order/', views.place_order, name='place_order'),
-    
+     path('menu-categories/', MenuCategoryListView.as_view(), name='menu-categories'),
 ]
  
