0c65c6c79625961e0930b7139fbd06668902f43e
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-20 13:34:28 +0530

    48122

--- home/models.py
+++ home/models.py
@@ -94,3 +94,10 @@ class Feedback(models.Model):
 
     def __str__(self):
         return f"Feedback from {self.name}"
+
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
