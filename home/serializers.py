6e3419b5469b377897e5dcef66c3261b76985158
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-24 00:35:57 +0530

    49794

--- home/serializers.py
+++ home/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
 from products.models import MenuCategory    
+from .models import MenuItem
 
 class MenuCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MenuCategory
         fields = ['id', 'name'] 
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField() 
+
+    class Meta:
+        model = MenuItem
+        fields = ["id", "name", "price", "category"] 
