3e0ac458b4c6f47141c25c85c7ec49241c28ecc1
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-24 01:06:28 +0530

    49798

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 import random
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Order
+from .serializers import OrderSerializer
+
 
 def order_confirmation(request):
     # Generate a sample order number (you can replace this with actual order id from DB)
@@ -9,3 +14,12 @@ def order_confirmation(request):
         'order_number': order_number
     }
     return render(request, 'orders/order_confirmation.html', context)
+
+    
+class OrderHistoryView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.filter(user=request.user).order_by("-created_at")
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data)
