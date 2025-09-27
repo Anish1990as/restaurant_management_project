3fd7a4707c48bc41c9379cfbb56989287cb3428a
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-09-27 19:13:26 +0530

    51676

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+from .views import UserProfileUpdateView
 
 urlpatterns = [
+    path('api/account/', include('account.urls')),
     path("profile/", profile_view, name="profile"),
+    path('profile/update/', UserProfileUpdateView.as_view(), name='profile-update'),
 ]
