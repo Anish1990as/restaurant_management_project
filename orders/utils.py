01556efc9bda00e68dcc0739a8918c7fdc69d49c
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-02 22:10:28 +0530

    52226

--- orders/utils.py
+++ orders/utils.py
@@ -1,6 +1,7 @@
 import string
 import secrets
 from .models import Coupon
+from .models import Order
 
 def generate_coupon_code(length=10):
     """Generate a unique alphanumeric coupon code."""
@@ -12,3 +13,15 @@ def generate_coupon_code(length=10):
         code = ''.join(secrets.choice(characters) for _ in range(length))
 
     return code
+
+
+def generate_unique_order_id(length=8):
+    """
+    Generate a unique alphanumeric ID for an Order.
+    Uses the secrets module for cryptographically secure random generation.
+    """
+    characters = string.ascii_uppercase + string.digits  # e.g. A-Z, 0-9
+    while True:
+        order_id = ''.join(secrets.choice(characters) for _ in range(length))
+        if not Order.objects.filter(order_id=order_id).exists():
+            return order_id
