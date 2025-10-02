8ef96e885b27de1a6e0942c8dd2df8c6c50dda9d
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-02 22:33:20 +0530

    52228

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -77,6 +77,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'restaurant_management.wsgi.application'
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "your_email@gmail.com"
+EMAIL_HOST_PASSWORD = "your_app_password"
+DEFAULT_FROM_EMAIL = "Your Project <your_email@gmail.com>"
+
 # Restaurant Name
 
 RESTAURANT_NAME = "Tasty Byte"
