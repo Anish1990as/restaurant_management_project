fc7966ce9fb6a5ac00575571fcbadeea3e33b8e4
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-02 22:08:00 +0530

    52230

--- home/models.py
+++ home/models.py
@@ -102,3 +102,11 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
