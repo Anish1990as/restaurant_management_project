3f3a2d129801edff5ce48d0537031d3953e755f2
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-14 11:48:52 +0530

    54660

--- orders/views.py
+++ orders/views.py
@@ -10,8 +10,10 @@ from utils.email_utils import send_order_confirmation_email
 from django.utils import timezone
 from .models import Coupon
 from .serializers import CouponSerializer
-
+from rest_framework import status
+from .serializers import OrderStatusUpdateSerializer
  
+
 class CancelOrderView(generics.UpdateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
@@ -118,3 +120,21 @@ class CouponValidationView(APIView):
             "code": coupon.code,
             "discount_percentage": float(coupon.discount_percentage)
         }, status=status.HTTP_200_OK)
+
+
+class UpdateOrderStatusAPIView(APIView):
+    def put(self, request, order_id):
+        try:
+            order = Order.objects.get(id=order_id)
+        except Order.DoesNotExist:
+            return Response({"error": "Order not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = OrderStatusUpdateSerializer(order, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "message": "Order status updated successfully.",
+                "order_id": order.id,
+                "new_status": serializer.data['status']
+            }, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
