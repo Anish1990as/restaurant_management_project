f883d30109897f9ee2f7fc0da3958e8cf276d1b8
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-18 21:35:23 +0530

    55555

--- home/models.py
+++ home/models.py
@@ -116,3 +116,10 @@ class MenuItem(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
