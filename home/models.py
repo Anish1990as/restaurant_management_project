d50cfac60e39dfd80215cbffd598595c53a5301f
Anish1990as <108995380+Anish1990as@users.noreply.github.com>
2025-10-18 21:41:22 +0530

    55554

--- home/models.py
+++ home/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from .models import MenuCategory
 
 class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -123,3 +124,14 @@ class MenuCategory(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reviews')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='reviews')
+    rating = models.IntegerField()
+    comment = models.TextField()
+    review_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.menu_item.name} ({self.rating}/5)"
